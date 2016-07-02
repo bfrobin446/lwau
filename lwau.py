6964394bf50990e5421966ee0c66efd66fe0b35f
Ben Robinson <bfrobin446@gmail.com>
2016-07-01 20:07:10 -0400

    Find real download link on GitHub or Spacedock

--- lwau.py
+++ lwau.py
@@ -1,13 +1,23 @@
 #! /usr/bin/env python3
 
+
+#--------------------------------------------------------------------------
+
+
 import codecs
 import functools
 import json
 import os
+import pathlib
 import sys
 import traceback
+import webbrowser
 
+from urllib.parse   import urlsplit, urlunsplit
 from urllib.request import urlopen
+
+
+#--------------------------------------------------------------------------
 
 
 # Whether we're checking for an update only or attempting to install, we need
@@ -23,15 +33,15 @@ class Mod:
                     **self.local_version_data["VERSION"])
 
             self.master_version_url = self.local_version_data["URL"]
-            self.master_version_data = json.load(
-                    codecs.getreader('utf-8-sig')(
-                        urlopen(self.master_version_url)))
+            self.master_version_data = json_load_from_url(
+                    self.master_version_url)
             self.available_version = Version(
                     **self.master_version_data["VERSION"])
         except Exception as e:
             self.exception = traceback.TracebackException.from_exception(e)
         else:
             self.exception = None
+
 
     def check_update(self):
         if self.exception:
@@ -46,6 +56,100 @@ class Mod:
             return self.available_version > self.installed_version
 
 
+    def update(self):
+        self.find_download()
+
+        if self.archive_url is None:
+            return False
+
+        return True
+
+
+    def find_download(self):
+        self.archive_url = None
+
+        if "GITHUB" in self.master_version_data:
+            self.archive_url = self.find_github_download()
+            if self.archive_url is not None:
+                return
+
+        if "DOWNLOAD" in self.master_version_data:
+            download_url = self.master_version_data["DOWNLOAD"]
+            if ("spacedock" in download_url):
+                self.archive_url = self.find_spacedock_download()
+                if self.archive_url is not None:
+                    return
+
+            if ("github" in download_url and "GITHUB" not in
+                    self.master_version_data):
+                download_path = pathlib.PurePosixPath(
+                        urlsplit(download_url)[2])
+                gh_user = download_path.parts[1]
+                gh_path = download_path.parts[2]
+                self.archive_url = self.find_github_download(gh_user, gh_path)
+                if self.archive_url is not None:
+                    return
+
+            print("Could not find an archive for {}".format(
+                download_url))
+            webbrowser.open(download_url, new=2)
+
+        else:
+            print("{} did not specify a download page.".format(
+                self.local_version_data["NAME"]))
+
+
+    def find_github_download(self, username=None, repo=None):
+        if username is None:
+            username = self.master_version_data["GITHUB"]["USERNAME"]
+        if repo is None:
+            repo = self.master_version_data["GITHUB"]["REPOSITORY"]
+        github_releases_url = (
+            "https://api.github.com/repos/{}/{}/releases".format(
+                username, repo))
+        github_releases_data = json_load_from_url(github_releases_url)
+
+        try:
+            wanted_release = next(r for r in github_releases_data
+                    if str(self.available_version) in r["tag_name"])
+        except StopIteration:
+            print('GitHub: No tag matching "{}"'.format(
+                    self.available_version))
+            return None
+
+        try:
+            wanted_asset = next(a for a in wanted_release["assets"]
+                    if a["name"].endswith(".zip"))
+        except StopIteration:
+            print('GitHub: No .zip archive for release "{}"'.format(
+                wanted_release["tag_name"]))
+            return None
+
+        return wanted_asset["browser_download_url"]
+
+
+    def find_spacedock_download(self):
+        download_page = urlsplit(self.master_version_data["DOWNLOAD"])
+        mod_id = pathlib.PurePosixPath(download_page.path).parts[2]
+        mod_data = json_load_from_url(
+                "http://spacedock.info/api/mod/{}".format(mod_id))
+
+        try:
+            wanted_version = next(v for v in mod_data["versions"]
+                    if self.available_version.fuzzy_equals(
+                        Version(str=v["friendly_version"])))
+        except StopIteration:
+            print('No Spacedock download for version "{}"'.format(
+                    self.available_version))
+            return None
+
+        return urlunsplit(('http','spacedock.info',
+                    wanted_version["download_path"], '', ''))
+
+
+#--------------------------------------------------------------------------
+
+
 # A version is a tuple of 1 to 4 non-negative integers, referred to as
 #  "major", "minor", "patch", and "build".
 # If fewer than four are given in the file, the remaining variables are set
@@ -55,11 +159,19 @@ class Version:
     # Our __init__ method accepts the keywords in all caps, as they appear in
     #  the .version file.
     def __init__(self,
-            major=None, minor=None, patch=None, build=None, **kwargs):
-        self.major = major or kwargs.get("MAJOR")
-        self.minor = minor or kwargs.get("MINOR")
-        self.patch = patch or kwargs.get("PATCH")
-        self.build = build or kwargs.get("BUILD")
+            major=None, minor=None, patch=None, build=None,
+            str=None, **kwargs):
+        if str is not None:
+            from itertools import zip_longest
+            parts = map(int, str.split('.'))
+            for member, val in zip_longest(("major", "minor", "patch",
+                "build"), parts):
+                setattr(self, member, val)
+        else:
+            self.major = major or kwargs.get("MAJOR")
+            self.minor = minor or kwargs.get("MINOR")
+            self.patch = patch or kwargs.get("PATCH")
+            self.build = build or kwargs.get("BUILD")
 
     # Equality is member-by-member. 
     def __eq__(self, other):
@@ -67,6 +179,15 @@ class Version:
                  and self.minor == other.minor
                  and self.patch == other.patch
                  and self.build == other.build )
+
+    def fuzzy_equals(self, other):
+        def numbers_fuzzy_equal(x, y):
+            return x == y or (x == 0 and y is None) or (x is None and y == 0)
+
+        return (     numbers_fuzzy_equal(self.major, other.major)
+                 and numbers_fuzzy_equal(self.minor, other.minor)
+                 and numbers_fuzzy_equal(self.patch, other.patch)
+                 and numbers_fuzzy_equal(self.build, other.build) )
 
     # Comparison is lexicographic. A position that is not used (None)
     #  compares less than one that is set to zero.
@@ -98,6 +219,9 @@ class Version:
     def __repr__(self):
         return "Version({major}, {minor}, {patch}, {build})".format(
                 **self.__dict__)
+
+
+#--------------------------------------------------------------------------
 
 
 def main():
@@ -136,12 +260,43 @@ def main():
             else:
                 sys.exit(0)
 
+    elif args.verb == "update":
+        if args.target == "all":
+            updates_failed = 0
+            for m in find_installed_mods():
+                if m.check_update():
+                    if not m.update():
+                        updates_failed += 1
+            if updates_failed:
+                print("{} mods need manual intervention.".format(
+                    updates_failed))
+                sys.exit(1)
+            else:
+                sys.exit(0)
+        else:
+            m = Mod(args.target)
+            if m.exception:
+                for s in m.exception.format():
+                    print(s, end='')
+                sys.exit(2)
+            elif m.check_update():
+                if m.update():
+                    sys.exit(0)
+                else:
+                    sys.exit(1)
+            else:
+                sys.exit(0)
+
 
 def find_installed_mods():
     for dir in os.walk("GameData"):
         for fname in dir[2]:
             if fname.endswith(".version"):
                 yield Mod(os.path.join(dir[0],fname))
+
+
+def json_load_from_url(u):
+    return json.load(codecs.getreader('utf-8-sig')(urlopen(u)))
 
 
 if __name__ == '__main__':
